647c1ee99c14a6b1f9b682599376ddc109b50c4c
Protegee <kujri111@gmail.com>
2023-02-16 15:37:24 +0700

    Make when user cancel "Change Size" button window prompt don't reset color of div.

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 const divContainer = document.querySelector(".container");
 const changeGridBtn = document.querySelector(".change-grid");
 
-let userNumber = 16;
+let defaultNumber = 16;
+let isMouseDown = false;
 
 const resetBtn = changeGridBtn.addEventListener("click", () => {
-  let userSize = prompt(
-    "Enter the number of squares you want to change.",
-    `${userNumber}`
+  let userSize = Number(
+    prompt(
+      "Enter the number of squares you want to change 0-100.",
+      `${defaultNumber}`
+    )
   );
 
-  while (userSize === null || userSize < 0) {
-    userSize = userNumber;
+  while (userSize > 100 || userSize < 0) {
+    userSize = prompt("Please pick a number 100 or less.", `${defaultNumber}`);
   }
 
-  while (userSize > 100) {
-    userSize = prompt("Pick a smaller number 100 or less.");
+  if (userSize === null) {
+    userSize = defaultNumber;
+  } else {
+    divContainer.style.gridTemplateRows = `repeat(${userSize}, 1fr)`;
+    divContainer.style.gridTemplateColumns = `repeat(${userSize}, 1fr)`;
+    defaultNumber = userSize;
+    divContainer.innerHTML = "";
+    createGrid(userSize);
   }
-  divContainer.style.gridTemplateRows = `repeat(${userSize}, 1fr)`;
-  divContainer.style.gridTemplateColumns = `repeat(${userSize}, 1fr)`;
-
-  userNumber = userSize;
-  divContainer.innerHTML = "";
-  createGrid(userSize);
+  console.log(userSize);
 });
 
 let createGrid = (number) => {
@@ -29,10 +33,11 @@ let createGrid = (number) => {
     const containerChild = document.createElement("div");
     divContainer.appendChild(containerChild);
     containerChild.classList.add("box");
-    containerChild.addEventListener("mousemove", () => {
+    // Makes background color div change when mouse over
+    containerChild.addEventListener("mouseenter", () => {
       containerChild.style.backgroundColor = "black";
     });
   }
 };
 
-createGrid(16);
+createGrid(defaultNumber);
